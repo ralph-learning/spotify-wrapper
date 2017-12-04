75b90ac70f42a2e58aa5c9e7de8b27682c71f994
Ralph EFfting <ralfting@gmail.com>
2017-12-04 12:35:16 -0200

    Add smoke tests

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchAlbuns = () => {};
+export const searchArtist = () => {};
+export const searchTracks = () => {};
+export const searchPlayList = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,26 @@
 import { expect } from 'chai';
+import { search, searchAlbuns, searchArtist, searchTracks, searchPlayList } from '../src/main';
 
-describe('Main', () => {
-  it('works', () => {
-    expect(true).to.be.equal(true);
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.be.a('function');
+    });
+
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.be.a('function');
+    });
+
+    it('should exist the searchArtist method', () => {
+      expect(searchArtist).to.be.a('function');
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.be.a('function');
+    });
+
+    it('should exist the searchPlayList method', () => {
+      expect(searchPlayList).to.be.a('function');
+    });
   });
 });
