342a8e625c87eb2d1a4a66a221dff8043dc685ee
Ralph Effting <ralfting@gmail.com>
2017-12-07 10:57:32 -0200

    Add example in html using UMD

--- examples/albums.js
+++ examples/albums.js
@@ -1,4 +1,14 @@
-import { searchAlbums } from '../src/search';
+import SpotifyWrapper from '../src/index';
 
-const albums = searchAlbums('Bon Jovi');
-albums.then(data => data.albums.items.map(item => console.log(item.name)));
+global.fetch = require('node-fetch');
+
+const spotify = new SpotifyWrapper({
+  token: 'BQC59542Qdvc0nfl2pr2_VituHBajvsToJGlEtba0Is1EOE8p1s17hoVAPmZG9iBtm4vTL7PcfmRaKGXAmhTyg-3NOqfvk30XHUpKURKhypaXUSFuKWOoLWDEI5PzSm2crk8_8sibL0',
+});
+
+const artist = spotify.search.artist('Metallica');
+
+artist
+  .then(response => response.json())
+  .then(data => data.artists.items.map(item => console.log(item.name)))
+  .catch(error => console.log(error));
